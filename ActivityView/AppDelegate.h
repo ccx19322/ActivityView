678f1258bd7883d529db6cf90ef6e08f8dbd9ac5
ccx19322 <chenchaoxu19322@163.com>
2017-02-20 10:01:45 +0800

    Initial Commit

---
+++ ActivityView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ActivityView
+//
+//  Created by chen cx on 2017/2/20.
+//  Copyright © 2017年 chen cx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
